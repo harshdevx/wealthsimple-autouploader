0ac7b2b0792374c46659fa9607612ab7e540ea9b
harshalc <business.harshal@gmail.com>
2025-01-04 07:37:19 -0500

    fixed date to yesterday to enable complete pull

--- src/main.py
+++ src/main.py
@@ -17,8 +17,9 @@ def main():
         ghostfolio = Ghostfolio()
 
         ws_accounts_list = wealth_simple.get_ws_accounts_list()
-        today = str(datetime.datetime.today().date())
-        wealth_simple.get_ws_data(today, today, ws_accounts_list)
+        today = datetime.datetime.today().date()
+        yesterday = today - datetime.timedelta(days=1)
+        wealth_simple.get_ws_data(yesterday, yesterday, ws_accounts_list)
 
         raw_data = wealth_simple.get_raw_ws_data()
 
